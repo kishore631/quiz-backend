ad306fd35079a9bf266e82f4595f5f24955d8e6f
GIDIGI VENKATA KISHORE <gidigivenkatakishore@gmail.com>
2026-04-17 17:15:15 +0530

    fix jwt env

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -42,7 +42,7 @@ router.post("/login", async (req, res) => {
 
     const token = jwt.sign(
       { id: user._id, role: user.role },
-      "secretKey",
+      process.env.JWT_SECRET,
       { expiresIn: "1d" }
     );
 
